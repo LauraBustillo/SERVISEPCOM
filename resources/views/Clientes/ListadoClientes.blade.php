513a85eda193bea92768dc9df75d41d1079515bf
MarioOrrego <mario.orrego@unah.hn>
2022-10-07 23:15:40 -0700

    Update ListadoClientes.blade.php
    Estilos de la lista de clientes

--- resources/views/Clientes/ListadoClientes.blade.php
+++ resources/views/Clientes/ListadoClientes.blade.php
@@ -1,6 +1,61 @@
 @extends('main')
 @section('extra-content')
+<style>
+ 
+.input-group-text  {
+  background-color: #B8D7F9;
+  border: 1.5px solid #000000;
+}
 
+.form-control  {
+    background-color: transparent;
+    border: 1.5px solid #000000;
+}
+
+.table-1{
+    width: 100%;
+    color: rgb(3, 10, 1);
+    border: 1px solid #000000;
+
+}
+
+.table-1 th{
+    background-color: #0319C4;
+    color:white;
+    padding: .2rem;
+    text-align: start;
+}
+
+.btn-info{
+    background-color: transparent;
+    border: 1px solid #000000;
+}
+
+/*Los titulos */ 
+.titulo {
+  font: italic normal bold normal 3em/1 Helvetica, Arial, sans-serif;
+  color: black;
+  font-family: 'Open Sans';
+  font-size: 20xp;
+}
+
+/*Los botones*/ 
+.btn-outline-dark {
+  background-color: transparent;
+  border: 1.8px solid #000000;
+  text-decoration: none;
+  color: black;
+  
+}
+
+/*Quitar subrayado*/ 
+ a:link, a:visited, a:active, a:focus{
+    text-decoration:none;
+    Color: black; 
+   
+  }
+
+</style>
 
 @if (session('mensaje'))
 <div class="alert alert-success">
@@ -57,6 +112,24 @@
     </tbody>
     </table>
   </div>
+
+  <Script>
+function busquedaJQsimple() {
+  var filtro = $("#buscar").val().toUpperCase();
+
+  $("#tabla tbody tr").each(function() {
+    texto = $(this).children("td:eq(0)").text().toUpperCase();
+    
+    if (texto.indexOf(filtro) < 0) {
+      $(this).hide();
+    } else{
+      $(this).show();
+    }
+    
+  });
+  
+}
+</Script>
 
 
 {{ $clientes->links() }}
