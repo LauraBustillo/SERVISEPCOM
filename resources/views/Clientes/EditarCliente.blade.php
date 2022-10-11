917518064919e5d3d3e2848ceb1770be698a5233
LauraBustillo <93621801+LauraBustillo@users.noreply.github.com>
2022-10-10 18:37:38 -0600

    Update EditarCliente.blade.php

--- resources/views/Clientes/EditarCliente.blade.php
+++ resources/views/Clientes/EditarCliente.blade.php
@@ -46,7 +46,7 @@ a { color: aliceblue;
 
 
 </style>
-<form class="form-control" id="Editar" name="Editar"  method="POST" style="text-align: center;" >
+<form class="form-control" id="form_editarC" name="form_editarC"  method="POST" style="text-align: center;" onsubmit="confirmar()" >
 @method('put')
 @csrf
 
@@ -106,4 +106,27 @@ a { color: aliceblue;
 </form>
 
 @endsection 
+{{--mensaje de confirmacion --}}
+@push('alertas')
+    <script>
+        function confirmar() {
+           var formul = document.getElementById("form_editarC");
+           
+           Swal.fire({
+                title: '¿Está seguro que desea guardar los cambios?',
+                icon: 'question',
+                confirmButtonColor: '#3085d6',
+                showCancelButton: true,
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si',
+                cancelButtonText: 'No'
+            }).then((result)=>{
+                if (result.isConfirmed) {
+                    formul.submit();
+                }
+            })
+            event.preventDefault()
+        }
+    </script>
+@endpush
 @include('common')
