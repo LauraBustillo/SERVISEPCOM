ad2013865c771534baca402e168003e1ab893813
EvaHerrera1 <eva.herrera@unah.hn>
2022-10-18 23:26:35 -0600

    Update Producto.php
    creación de relación en productos

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
     use HasFactory;
+    public function proveedor(){
+        return $this-> belongsTo(Proveedor::class);
+     }
+
+     public function categoria(){
+        return $this-> belongsTo(Categoria::class);
+     }
 }
