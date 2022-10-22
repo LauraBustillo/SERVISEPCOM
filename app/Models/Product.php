ce1367b41bd320f1afa6da7465689247557ec2a4
EvaHerrera1 <eva.herrera@unah.hn>
2022-10-21 19:24:29 -0600

    Create Product.php

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    public function proveedors(){
+        return $this-> belongsTo(Proveedor::class, 'proveedor_id');
+     }
+
+     public function categorias(){
+        return $this-> belongsTo(Categoria::class, 'categoria_id');
+     }
+}
