7edb1a0af7112fe350ba1535ad89660034c7b3bc
EvaHerrera1 <eva.herrera@unah.hn>
2022-10-21 19:25:32 -0600

    Create 2022_10_19_020035_create_products_table.php

---
+++ database/migrations/2022_10_19_020035_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('proveedor_id');
+            $table->foreign('proveedor_id')->references('id')->on('proveedors'); 
+            $table->string('Nombre_producto');
+            $table->string('Descripcion');
+            $table->string('Marca');
+            $table->unsignedBigInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->string('Cantidad');
+            $table->integer('Precio_compra');
+            $table->integer('Precio_venta');
+            $table->float('Impuesto');
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
