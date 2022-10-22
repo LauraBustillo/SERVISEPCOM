49e4e3441aaf40f763b388e485c4229d17a14d7e
EvaHerrera1 <eva.herrera@unah.hn>
2022-10-21 19:25:39 -0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(ProveedorSeeder::class);
+        $this->call(CategoriaSeeder::class);
+        $this->call(PoductSeeder::class);
+        $this->call(EmpleadoSeeder::class);
+        $this->call(ClienteSeeder::class);
     }
 }
