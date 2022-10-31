d22d658f6689021bda9e7323985eb7e79a8c143d
LauraBustillo <93621801+LauraBustillo@users.noreply.github.com>
2022-10-30 18:24:46 -0600

    Update EditarProveedor.blade.php

--- resources/views/Proveedores/EditarProveedor.blade.php
+++ resources/views/Proveedores/EditarProveedor.blade.php
@@ -80,13 +80,16 @@ a { color: aliceblue;
             <div class="row g-3" class="input-group input-group-sm mb-1" >
               <div class="col">
             
-                <input type="text"   minlength="3" maxlength="25" name="Nombre_empresa" id="Nombre_empresa"  pattern="[A-ZÑ a-zñ]+" class="form-control"  required title="Este campo solo debe de contener letras" placeholder="Nombre de la empresa" 
-                aria-label="First name" value="{{$modificar->Nombre_empresa}}">
+                <input type="text"   minlength="3" maxlength="25" name="Nombre_empresa" id="Nombre_empresa"  
+                pattern="[A-ZÑ a-zñ]+" class="form-control"  required title=" Solo debe de tener letras"
+                placeholder="Nombre de la empresa" 
+                aria-label="First name" value="{{old('Nombre_empresa', $modificar->Nombre_empresa)}}" >
               </div>
               <div class="col">
-                <input type="text" minlength="4" maxlength="25" name="Correo" id="Correo"  class="form-control"   required pattern="[a-zA-Z0-9!#$%&'*_+-]([\.]?[a-zA-Z0-9!#$%&'*_+-])+@[a-zA-Z0-9]([^@&%$\/()=?¿!.,:;]|\d)+[a-zA-Z0-9][\.][a-zA-Z]{2,4}([\.][a-zA-Z]{2})?"
+                <input type="text" minlength="4" maxlength="25" name="Correo" id="Correo"  class="form-control" 
+                  required pattern="[a-zA-Z0-9!#$%&'*_+-]([\.]?[a-zA-Z0-9!#$%&'*_+-])+@[a-zA-Z0-9]([^@&%$\/()=?¿!.,:;]|\d)+[a-zA-Z0-9][\.][a-zA-Z]{2,4}([\.][a-zA-Z]{2})?"
 
-                 placeholder="Correo" aria-label="gmail" value="{{$modificar->Correo}}">
+                 placeholder="Ingrese el correo electrónico" aria-label="gmail" value="{{old('Correo',$modificar->Correo)}}">
               </div>
             </div>
             <br> 
@@ -96,28 +99,33 @@ a { color: aliceblue;
                 <div class="col"  >
                     <span class="input-group-text" style="width: 109%"  id="inputGroup-sizing-sm">Teléfono de la empresa</span>
                     <input type="text" style="width: 109%"  minlength="8" maxlength="8" name="Telefono_empresa" id="Telefono_empresa" class="form-control" aria-label="Sizing example input"
-                    aria-describedby="inputGroup-sizing-sm" required title="Primer digito (2, 3, 8 o 9) y solo debe contener números."  pattern="([9,8,3,2]{1}[0-9]{7})" pattern="[0-9]+" 
-                    placeholder="Eje. 00000000" value="{{$modificar->Telefono_empresa}}">
+                    aria-describedby="inputGroup-sizing-sm" required title="Primer digito (2, 3, 8 o 9) y solo debe tener números."  pattern="([9,8,3,2]{1}[0-9]{7})" pattern="[0-9]+" 
+                    placeholder="Eje. 00000000" value="{{old('Telefono_empresa', $modificar->Telefono_empresa)}}">
+
+                   
                 </div> 
                 
                 {{-- Direccion --}}
                 <div class="col" style="padding-left: 6%" > 
                     <span class="input-group-text"  style="width: 109%">Dirección </span>
                     <textarea class="form-control ancho-alto" spellcheck="true" minlength="10" maxlength="150" 
-                    name="Direccion" id="Direccion" id="exampleFormControlTextarea1"  required placeholder="Ingrese la dirección exacta de la empresa">{{$modificar->Direccion}}</textarea>
+                    name="Direccion" id="Direccion" id="exampleFormControlTextarea1"  required placeholder="Ingrese la dirección exacta de la empresa">{{old('Direccion',$modificar->Direccion)}}</textarea>
                 </div>
                 </div>
 
                 {{-- Nombres y Apellidos del encargado--}}
             <div class="row g-3">
                 <div class="col">
-                <input type="text"   minlength="3" maxlength="25" name="Nombre_encargado" id="Nombre_encargado"  pattern="[A-ZÑ a-zñ]+" class="form-control"  required title="Este campo solo debe de contener letras" placeholder="Nombres del encargado" 
-                aria-label="First name" value="{{$modificar->Nombre_encargado}}">
+                <input type="text"   minlength="3" maxlength="25" name="Nombre_encargado" id="Nombre_encargado"  pattern="[A-ZÑ a-zñ]+" 
+                class="form-control"  required title="Solo debe de tener letras" placeholder="Nombres del encargado" 
+                aria-label="First name" value="{{old('Nombre_encargado', $modificar->Nombre_encargado)}}">
                 </div>
 
                 <div class="col">
-                <input type="text" minlength="4" maxlength="25" name="Apellido_encargado" id="Apellido_encargado" pattern="[A-ZÑ a-zñ]+" class="form-control"   required title="Este campo solo debe de contener letras"
-                placeholder="Apellidos del encargado" aria-label="last name" value="{{$modificar->Apellido_encargado}}">
+                <input type="text" minlength="4" maxlength="25" name="Apellido_encargado" id="Apellido_encargado" 
+                pattern="[A-ZÑ a-zñ]+" class="form-control"   required title="Solo debe de tener letras"
+                placeholder="Apellidos del encargado" aria-label="last name" 
+                value="{{old('Apellido_encargado',$modificar->Apellido_encargado)}}">
                 </div>
             </div>
             <br>
@@ -125,9 +133,11 @@ a { color: aliceblue;
             
             <div class="col" style="padding-left:27%" > 
                 <span class="input-group-text" style="width: 60%"  id="inputGroup-sizing-sm">Teléfono del encargado</span>
-                <input type="text" style="width: 60%"   minlength="8" maxlength="8" name="Telefono_encargado" id="Telefono_encargado" class="form-control" aria-label="Sizing example input"
-                aria-describedby="inputGroup-sizing-sm" required title="Primer digito (2, 3, 8 o 9) y solo debe contener números."  pattern="([9,8,3,2]{1}[0-9]{7})" pattern="[0-9]+" 
-                placeholder="Eje. 00000000" value="{{$modificar->Telefono_encargado}}">
+                <input type="text" style="width: 60%"   minlength="8" maxlength="8" name="Telefono_encargado" id="Telefono_encargado"
+                 class="form-control" aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-sm" required title="Primer digito (2, 3, 8 o 9) y solo debe tener números."  
+                pattern="([9,8,3,2]{1}[0-9]{7})" pattern="[0-9]+" 
+                placeholder="Eje. 00000000" value="{{old('Telefono_encargado',$modificar->Telefono_encargado)}}">
             </div>
 
             <br>
