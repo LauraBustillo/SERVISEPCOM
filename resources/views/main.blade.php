a4615fe6589d42a31a98dd61748da687c9c89098
LauraBustillo <93621801+LauraBustillo@users.noreply.github.com>
2022-11-05 16:10:24 -0600

    index

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -26,7 +26,7 @@
 
     {{-- icons --}}
     {{-- https://fonts.google.com/icons --}}  
-    <title>SEPCOM</title>
+    <title>SEPCOM Mario</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
