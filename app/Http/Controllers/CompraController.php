b6624509c766a5a7012f50b8a4240c8b1bd69df5
LauraBustillo <93621801+LauraBustillo@users.noreply.github.com>
2022-11-05 13:43:45 -0600

    Update CompraController.php

--- app/Http/Controllers/CompraController.php
+++ app/Http/Controllers/CompraController.php
@@ -11,33 +11,40 @@ use Carbon\Carbon;
 class CompraController extends Controller
 {
   /*Funcion para  guardar  */
-  public function index(){
-    $compras = Compra::orderBy('created_at', 'DESC')->paginate(10);
+public function index(Request $request){
+  $compras = [];
+  $buscar = '';
+  
+  if($request->buscar != null && $request->buscar != ''){
+    $buscar = $request->buscar;
+    $compras =  Compra::where(DB::raw ('Numero_factura'), "like","%".strtolower($request->buscar)."%")
+    ->orwhere('Fecha_facturacion', 'like','%'.strtolower($request->buscar).'%')->paginate(10); 
+   
+  }else{
     $buscar = '';
-    return view('Compras.ListadoCompras' )->with('compras', $compras)->with('buscar', $buscar);
-    
+    $compras = Compra::paginate(10);
+  }
 
+    return view('Compras.ListadoCompras' )->with('compras', $compras)->with('buscar', $buscar);
 }
 
-    /*Funcion para  guardar  */
-    public function guardar(){
-        return view('Compras.RegistroCompras');
-   }
-
-   /*Funcion para  guardar  */
-   public function show(){
-    // $products = Product::table('')->join();
+  /*Funcion para  guardar  */
+  // public function guardar(){
+  //     return view('Compras.RegistroCompras');
+  // }
 
 
+/*Funcion para  guardar  */
+public function show(){
 
     $query = 'SELECT p.id as id_product, cat.id as id_cat, prov.id as id_prov, p.Nombre_producto,p.Descripcion,
-    p.Marca,p.Precio_compra,p.Precio_venta,p.Cantidad, p.Impuesto, prov.Nombre_empresa, cat.Descripcion as DescripcionC  
+    p.Marca,p.Precio_compra,p.Precio_venta,p.Cantidad, p.Impuesto, prov.Nombre_empresa, 
+    cat.Descripcion as DescripcionC  
     FROM products AS p
     INNER JOIN proveedors  as prov ON p.proveedor_id = prov.id
     INNER JOIN categorias AS cat ON p.categoria_id = cat.id;';
     $accion = 'guardar';
     $today = Carbon::now()->format('Y-m-d');
-    // return $today;
 
     $detallefactura = [];
     $factura = array("Numero_factura"=>"","Fecha_facturacion"=>$today,"Total_factura"=>0,"Telefono_proveedor"=>'',"Proveedor"=>"");
@@ -51,8 +58,9 @@ class CompraController extends Controller
     ->with('factura',$factura);
 }
 
-  public function guardarFactura($arrayFac,$arrayDet){
+public function guardarFactura($arrayFac,$arrayDet){
  
+  // pasamos los string parametros a arreglos
   $jsonFactura =  json_decode($arrayFac);
   $arrayDetallesFac =  json_decode($arrayDet);
 
@@ -65,9 +73,10 @@ class CompraController extends Controller
 
   foreach ($arrayDetallesFac as $detFact) {
     $a = new CompraDetalles;
+    $a->id_detalle= $detFact->id_detalle;
     $a->Numero_facturaform = $detFact->Numero_facturaform;
     $a->id_prov = $detFact->id_prov;
-   
+
     $a->id_product = $detFact->id_product ;
     $a->nombre_producto = $detFact->nombre_producto ;
     $a->Descripcion = $detFact->Descripcion ;
@@ -86,10 +95,11 @@ class CompraController extends Controller
   return redirect()->route('compra.index');
 }
 
+
+  //Funcion para actualizar
   public function actualizarFactura($arrayFac,$arrayDet){
   $jsonFactura =  json_decode($arrayFac);
   $arrayDetallesFac =  json_decode($arrayDet);
-
   $actu = Compra::find($jsonFactura->id);
   $actu -> Fecha_facturacion = $jsonFactura -> Fecha_facturacion;
   $actu -> Numero_factura = $jsonFactura -> Numero_factura;
@@ -101,9 +111,9 @@ class CompraController extends Controller
 
   foreach ($arrayDetallesFac as $detFact) {
     $a = new CompraDetalles;
+    $a->id_detalle= $detFact ->id_detalle;
     $a->Numero_facturaform = $jsonFactura -> Numero_factura;
     $a->id_prov = $detFact->id_prov ;
-    
     $a->id_product = $detFact->id_product ;
     $a->nombre_producto = $detFact->nombre_producto ;
     $a->Descripcion = $detFact->Descripcion ;
@@ -122,17 +132,18 @@ class CompraController extends Controller
   // return redirect()->route('compra.index');
 }
 
+  //Funcion para editar
+    public function comprasEdit($id){
 
-public function comprasEdit($id){
-  $query = 'SELECT p.id as id_product, cat.id as id_cat, prov.id as id_prov, p.Nombre_producto,p.Descripcion,p.Marca,p.Precio_compra,p.Precio_venta,p.Cantidad, p.Impuesto, prov.Nombre_empresa,cat.Descripcion as DescripcionC  FROM products AS p
+    $query = 'SELECT p.id as id_product, cat.id as id_cat, prov.id as id_prov, p.Nombre_producto,p.Descripcion,p.Marca,p.Precio_compra,p.Precio_venta,p.Cantidad, p.Impuesto, prov.Nombre_empresa,cat.Descripcion as DescripcionC  FROM products AS p
     INNER JOIN proveedors  as prov ON p.proveedor_id = prov.id
     INNER JOIN categorias AS cat ON p.categoria_id = cat.id;';
+    $products = DB::select(DB::raw($query));
+
     $factura = Compra::where('id','=',$id)->first();
     $detallefactura = CompraDetalles::where('Numero_facturaform','=',$factura->Numero_factura)->get();
     $proveedores = Proveedor::all();
-    $products = DB::select(DB::raw($query));
     $accion = 'editar';
-
 
     return view('Compras.RegistroCompras')
     ->with('products',$products)
@@ -141,47 +152,29 @@ public function comprasEdit($id){
     ->with('factura',$factura)
     ->with('detallefactura',$detallefactura);
 }
-   /* Validar  guardar  */
-   public function agg(Request $request){
-
-     $rules= ([
-       'Numero_factura' =>'required|numeric|min:11|max:11',
-       'Fecha_facturacion' =>'required|date',
-       'Total_factura' =>'required|numeric',
-     ]);
-     
-
-    $mesaje=([
-
-    'Numero_factura.required'=>'El número de factura es obligatorio',
-    'Numero_factura.numeric'=>'El número de factura solo debe contener números',
-    'Numero_factura.min'=>'El número de factura debe contener minimo 8 números',
-    'Numero_factura.max'=>'El número de factura debe contener máximo 8 números',
-
-    'Fecha_facturacion.required'=>'La fecha de factura es obligatorio',
-    'Fecha_facturacion.numeric'=>'La fecha de factura solo debe contener números',
-
-    'Total_factura.required'=>'El total de la factura es obligatorio',
-    'Total_factura.numeric'=>'El total de la factura solo debe contener números',
-    'Total_factura.min'=>'El total de la factura debe contener minimo 3 números',
-    'Total_factura.max'=>'El total de la factura debe contener máximo 5 números',
-   ]);
-
-   $this->validate($request, $rules, $mesaje);
-
-  $agregar = new Compra();
-
-  $agregar -> Numero_factura = $request->input('Numero_factura');
-  $agregar -> Fecha_facturacion = $request->input('Fecha_facturacion');
-  $agregar -> Total_factura = $request->input('Total_factura');
-
-  $agregar1 =  $agregar->save();
 
 
- if ($agregar1){
-    return redirect()->route('show.registroCompras')->with('mensaje', 'Se guardó  con  éxito') ;
- } else {
- 
+
+/*
+|--------------------------------------------------------------------------
+| IVENTARIO
+|--------------------------------------------------------------------------
+*/
+
+
+
+   /*Funcion para mostrar mas informacion  */ 
+   public function mirar($id){
+    $factura = Compra::findOrFail($id);
+    return view('Inventario.InformacionInventario')->with('factura', $factura);
 }
-} 
+
+/*Funcion para mostar el historial de precios*/
+
+  /*Funcion para mostrar mas informacion  */ 
+  public function historial(){
+    /*$his = Compra::findOrFail($id);*/
+    return view('HistorialPrecios.HistorialPrecios');
+}
+
 }
