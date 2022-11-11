ebaf0398df1c54636d4064f01744aeb3ab5c5bda
LauraBustillo <93621801+LauraBustillo@users.noreply.github.com>
2022-11-10 18:37:44 -0600

    Update InventarioController.php

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -1,11 +1,12 @@
 <?php
-
 namespace App\Http\Controllers;
 use App\Models\Inventario;
 use App\Models\Compra;
 use App\Models\CompraDetalles;
+use App\Models\Product;
 use App\Models\Proveedor;
 use Illuminate\Http\Request;
+
 use DB;
 
 
@@ -13,22 +14,8 @@ class InventarioController extends Controller
 {
      
  /*Funcion para el listado  el buscador*/ 
- public function index(Request $request){
-      $inventario= [];
-      $buscar = '';
-
-      if($request->buscar != null && $request->buscar != ''){
-        $buscar = $request->buscar;
-        $inventario =  Inventario::where(DB::raw("LOWER(concat(Nombre,' ',Apellido))"),"like","%".strtolower($request->buscar)."%")
-        ->orwhere('Numero_identidad', 'like','%'.strtolower($request->buscar).'%')
-        ->paginate(10); 
-      }else{
-        $buscar = '';
-        $inventario = Inventario::paginate(10);
-      }
-
-      return view('Inventario.Inventario')->with('inventario', $inventario)->with('buscar',$buscar);
-  }
+ 
+ 
 
 
 
@@ -40,6 +27,5 @@ class InventarioController extends Controller
 
          
       }
-
 
 
