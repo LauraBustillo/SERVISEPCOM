c50d37d20b698b650bda4a113deba8f2572aa479
MarioOrrego <mario.orrego@unah.hn>
2022-11-23 22:09:26 -0600

    Update Cliente.php
    Relación cliente con mantenimiento.

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+    public function mantenimientos(){
+        return $this-> hasMany(Mantenimiento::class);
+    }
+
 }
