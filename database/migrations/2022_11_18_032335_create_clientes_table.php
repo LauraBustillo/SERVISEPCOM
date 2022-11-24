b04a6e5b6adb5a6c28a3747259a65376eed4d9d2
LauraBustillo <93621801+LauraBustillo@users.noreply.github.com>
2022-11-23 22:15:28 -0600

    Create 2022_11_18_032335_create_clientes_table.php
    Migración clientes

---
+++ database/migrations/2022_11_18_032335_create_clientes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre')->nullable();
+            $table->string('Apellido')->nullable();
+            $table->string('Numero_identidad')->nullable()->unique();
+            $table->string('Numero_telefono')->nullable()->unique();
+            $table->string('Direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
