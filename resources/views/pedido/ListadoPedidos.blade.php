cc22a95fb9f16c36c18bfa42dc86db6f222d1e67
LauraBustillo <93621801+LauraBustillo@users.noreply.github.com>
2022-11-23 22:16:20 -0600

    Update ListadoPedidos.blade.php

--- resources/views/pedido/ListadoPedidos.blade.php
+++ resources/views/pedido/ListadoPedidos.blade.php
@@ -54,8 +54,126 @@
     Color: black; 
    
   }
+  div.dataTables_wrapper div.dataTables_filter input {
+
+display: inline-block;
+width: 120% !important;
+background-color: transparent;
+   border: 1.5px solid #000000;
+   float: left;
+}
+.dataTables_wrapper .dataTables_filter {
+  float: left !important ;
+  text-align: left !important;
+width: 100% !important;
+
+}
 
 </style>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/datetime/1.2.0/css/dataTables.dateTime.min.css">
+
+<script  src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
+<script  src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+<script  src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
+<script  src="https://cdn.datatables.net/datetime/1.2.0/js/dataTables.dateTime.min.js"></script>
+<script  src="https://cdn.datatables.net/plug-ins/1.13.1/api/sum().js"></script>
+
+<br>
+
+<script>
+
+  $().ready(function(){
+  $('#myTable').DataTable({
+   language:{ "sProcessing": "Procesando...",
+        "sLengthMenu": "",
+        "sZeroRecords": "No se encontraron resultados",
+        "sEmptyTable": "",
+        "sInfo": "",
+        "sInfoEmpty": "",
+        "sInfoFiltered": "",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar por número de pedido",
+        "sUrl": ".",
+        "sInfoThousands": "",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        },
+        "buttons": {
+            "copy": "Copiar",
+            "colvis": "Visibilidad"
+        }
+    }
+
+  });
+  
+
+
+});
+
+var minDate, maxDate;
+
+// Custom filtering function which will search data in column four between two values
+
+
+$.fn.dataTable.ext.search.push(
+    function( settings, data, dataIndex ) {
+
+
+       var min = minDate.val();
+        var max = maxDate.val();
+        var date = new Date( data[1] );
+ 
+        if (
+            ( min === null && max === null ) ||
+            ( min === null && date <= max ) ||
+            ( min <= date   && max === null ) ||
+            ( min <= date   && date <= max )
+        ) {
+            return true;
+        }
+        return false;
+    }
+);
+ 
+$(document).ready(function() {
+    // Create date inputs
+    minDate = new DateTime($('#min'), {
+        format: 'YYYY - M - DD'
+    });
+    maxDate = new DateTime($('#max'), {
+        format:  'YYYY - M - DD'
+
+       
+    });
+ 
+    // DataTables initialisation
+
+    var table = $('#myTable').DataTable();
+
+    // Refilter the table
+    $('#min, #max').on('change', function () {
+        table.draw();
+
+
+    });
+});
+
+
+</script>
+
+
 
 
 @if (session('mensaje'))
@@ -73,7 +191,7 @@
 
 {{-- Buscador--}}
 <h1 class="titulo" style="text-align:center">Listado de pedidos</h1> 
-<br>
+{{-- <br>
 <nav class="navbar navbar-nav bg-nav" >
   <div class="container-fluid" >
      <form class="d-flex" id="ablescroll" method="POST" action="pedidos">
@@ -82,15 +200,38 @@
       <button  type="submit" class="btn btn-outline-dark me-2" id="buscar" name="buscador" value=" "><i class="bi bi-search"> </i></button>
       <a href="{{ route('index.pedido') }}" class="btn btn-outline-dark" ><i class="bi bi-x-square"></i></a>
     </form> 
-         <a    class="btn btn-outline-dark" style="float:right" href="{{route('create.pedido')}}" >
-         <i class="bi bi-person-plus-fill"> Nuevo pedido </i></a>
+
   </div>
-  </nav>
+  </nav> --}}
 <br>
 
+<div class="input-group " style="padding-right:4%"  style="width: 100%" ><br>
+  <div >
+  <label for="" class="group-text">Fecha minima de pedido:</label>
+  <input  class="form-control" id="min" name="min"> 
+  </div>&nbsp; &nbsp;&nbsp;
+  
+  <div >
+    <label for="" class="group-text">Fecha  máxima de pedido:</label>
+    <input   class="form-control" id="max" name="max" > 
+    </div>
+    <div><br>&nbsp; &nbsp;
+    <a href="{{ route('index.pedido') }}" class="btn btn-outline-dark" ><i class="bi bi-x-square"></i></a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <a    class="btn btn-outline-dark" style="float:right" href="{{route('create.pedido')}}" >
+      <i class="bi bi-person-plus-fill"> Nuevo pedido </i></a>
+ 
+  </div>
+
+  </div>   
+  <br>
 <div>    
 
-    <table class="table table-hover" id="tabla">
+    <table id='myTable' class="table table-hover">
         <thead>
         <tr>
             <th scope="col">Número pedido</th>
@@ -99,7 +240,6 @@
             <th scope="col">Estado</th>
             <th scope="col">Detalles</th>
             <th scope="col">Editar pedido</th>
-            <th scope="col"></th>
         </tr>
         </thead>
 
@@ -108,7 +248,7 @@
         <tr>
             <td>{{ $p->numero_pedido }}</td>
             <td>{{ $p->fecha_pedido }}</td>
-            <td>{{ $p->fecha_recibido == null ? 'No recibido': $p->fecha_recibido}}</td>
+            <td>{{ $p->fecha_recibido == null ? '----------': $p->fecha_recibido}}</td>
             <td>{{  $p->estado == 0 ? 'Pendiente':'Recibido' }}</td>
             <td><a class="btn-detalles" href="{{route('pedido.mostrar' , ['id' => $p->id]) }}"> <i class="bi bi-file-text-fill"> Detalles </i> </a></td>
 
@@ -121,6 +261,8 @@
   </div>
 
 {{ $pedidos->links() }}
+
+
 
 
 @endsection
