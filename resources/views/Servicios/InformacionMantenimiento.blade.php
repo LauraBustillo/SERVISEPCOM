cf0abda396ae46c225c108ba54faf8fdfa9f5d00
MarioOrrego <mario.orrego@unah.hn>
2022-11-23 22:07:13 -0600

    Update InformacionMantenimiento.blade.php
    Actualización de los detalles del mantenimiento.

--- resources/views/Servicios/InformacionMantenimiento.blade.php
+++ resources/views/Servicios/InformacionMantenimiento.blade.php
@@ -34,7 +34,68 @@
 }
 </style>
 
+<h1 class="titulo1">Información del matenimiento</h1>
+<br>
+<div>    
+    <div class="titulo" >Mantenimiento: {{ $detalle->Nombre }} {{$detalle->Apellido}}</div>
+<br>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Datos</th>
+                <th>Información</th>
+            </tr>
+        </thead>
 
+        <tbody>
+            <tr>
+                <td>Número de identidad</td>
+                <td>{{$detalle->Identidad}}</td>
+            </tr> 
 
+            <tr>
+                <td>Teléfono</td>
+                <td>{{$detalle->Telefono}}</td> 
+            </tr> 
+
+            <tr>
+                <td>Dirección</td>
+                <td>{{$detalle->Direccion}}</td>
+            </tr> 
+
+            <tr>
+                <td>Categoría</td>
+                <td>{{$detalle->categoria}}</td>
+            </tr> 
+            <tr>
+                <td>Nombre equipo</td>
+                <td>{{$detalle->nombre_equipo}}</td>
+            </tr> 
+
+            <tr>
+                <td>Marca</td>
+                <td>{{$detalle->marca}}</td>
+            </tr>
+            <tr>
+                <td>Modelo</td>
+                <td>{{$detalle->modelo}}</td>
+            </tr>
+            <tr>
+                <td>Fecha ingreso</td>
+                <td>{{$detalle->fecha_ingreso}}</td>
+            </tr>
+            <tr>
+                <td>Fecha entrega</td>
+                <td>{{$detalle->fecha_entrega}}</td>
+            </tr>
+            
+        </tbody>
+    </table>
+  </div>
+  
+  {{--Botones --}}
+  <a class="btn btn-outline-dark" href="{{route('mantenimiento.mostrar' , ['id' => $detalle->id]) }}"> <i class="bi bi-pen-fill"> Editar </i></a>
+  <a class="btn btn-outline-dark" href="{{route('mantenimiento.index')}}" ><i class="bi bi-x-circle-fill"> Volver </i></a>
+  
 @endsection
 @include('common')
