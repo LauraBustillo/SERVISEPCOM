0825804cbfcdf8900f9256ce8f9ce66deeab975f
EvaHerrera1 <eva.herrera@unah.hn>
2022-12-08 23:44:27 -0600

    Update MantenimientoController.php

--- app/Http/Controllers/MantenimientoController.php
+++ app/Http/Controllers/MantenimientoController.php
@@ -23,27 +23,28 @@ class MantenimientoController extends Controller
       $accion = 'agregar';
       $mantenimiento = (object)array("cliente_id"=>"", "Nombre"=>"", "Apellido"=>"", "Numero_telefono"=>"", "Numero_identidad"=>"","Direccion"=>"", 
       "numero_factura"=>"", "fecha_facturacion"=>"", "precio"=>"", "descripcion"=>"",
-      "estado"=>"", "categoria"=>"", "nombre_equipo"=>"", "marca"=>"", "modelo"=>"",
+      "estado"=>"",  "descripcionm"=>"", "categoria"=>"", "nombre_equipo"=>"", "marca"=>"", "modelo"=>"",
 
       "fecha_ingreso"=>"","fecha_entrega"=>"");
       // return    $mantenimiento ;
       $clientes =DB::table('clientes')->orderBy('id','DESC')->get();
-      return view('Servicios.RegistroMantenimiento')->with('clientes',$clientes)->with('mantenimiento',$mantenimiento)->with('accion',$accion);;
+      return view('Servicios.RegistroMantenimiento')->with('clientes',$clientes)->with('mantenimiento',$mantenimiento)->with('accion',$accion);
     }
 
     
-
+    
 
     /*Funcion para  guardar  */
     public function guardar(Request $request){
 
-      $rules= ([
+       $rules= ([
    
         'cliente_id'=>'required',
         'categoria'=>'required',
-        'nombre_equipo'=>'required|min:4|max:25',
-         'marca' =>'required|min:2|max:25',
-         'modelo'=>'required|min:4|max:25',
+        'nombre_equipo'=>'required|regex:/^([a-zñA-ZÑ]+)(\s[a-zñA-ZÑ]+)*$/|min:4|max:20',
+         'marca' =>'required|regex:/^([a-zñA-ZÑ0-9]+)(\s[a-zñA-ZÑ0-9]+)*$/|min:2|max:20',
+         'modelo'=>'required|regex:/^([a-zñA-ZÑ0-9]+)(\s[a-zñA-ZÑ0-9]+)*$/|min:4|max:20',
+         'descripcionm'=>'required|regex:/^([a-zñA-ZÑ0-9]+)(\s[a-zñA-ZÑ0-9]+)*$/|min:4|max:100',
          'fecha_ingreso'=>'required',
          'fecha_entrega'=>'required',
     
@@ -54,16 +55,22 @@ class MantenimientoController extends Controller
       'categoria.required'=>'La categoría es requerida',
     
        'nombre_equipo.required'=>'El nombre del equipo es requerido' ,
-       'nombre_equipo.min'=>'El nombre del equipo debe tener minimo 4 letras' ,
+       'nombre_equipo.regex'=>'El nombre del equipo solo debe tener letras' ,
+       'nombre_equipo.min'=>'El nombre del equipo debe tener mínimo 4 letras' ,
        'nombre_equipo.max'=>'El nombre del equipo  no debe de tener más de 25 letras' ,
     
        'marca.required'=>'La marca es requerida' ,
-       'marca.min'=>'La marca debe tener como minimo 2 letras' ,
+       'marca.min'=>'La marca debe tener como mínimo 2 letras' ,
        'marca.max'=>'La marca no debe de tener más de 25 letras' ,
-    
+       'marca.regex'=>'La marca solo puede tener letras y números' ,
+
        'modelo.required'=>'El modelo es requerido' ,
-       'modelo.min'=>'El modelo debe tener como minimo 4 letras' ,
-       'modelo.max'=>'El modelo no debe de tener más de 25 letras' ,
+       'modelo.regex'=>'El modelo solo puede tener letras y números' ,
+
+       'descripcionm.required'=>'La descripción es requerido' ,
+       'descripcionm.min'=>'La descripción debe tener como mínimo 4 letras' ,
+       'descripcionm.max'=>'La descripción debede tener más de 100 letras' ,
+       'descripcionm.regex'=>'La descripción solo puede tener letras y números' ,
     
        'fecha_ingreso.required'=>'La fecha de ingreso es requerida' ,
     
@@ -80,6 +87,7 @@ class MantenimientoController extends Controller
       $agregar->nombre_equipo = $request -> input('nombre_equipo');
       $agregar->marca = $request -> input('marca');
       $agregar->modelo = $request -> input('modelo');
+      $agregar->descripcionm = $request -> input('descripcionm');
       $agregar->fecha_ingreso = $request -> input('fecha_ingreso');
       $agregar->fecha_entrega = $request -> input('fecha_entrega');
       $agregar1=  $agregar->save();
@@ -102,6 +110,7 @@ public function actualizarMantenimiento(Request $request){
   $actu->nombre_equipo = $request->data['nombre_equipo'];
   $actu->marca = $request->data['marca'];
   $actu->modelo = $request->data['modelo'];
+  $actu->descripcionm = $request -> input('descripcionm');
   $actu->fecha_ingreso = $request->data['fecha_ingreso'];
   $actu->fecha_entrega = $request->data['fecha_entrega'];
   $agregar = $actu -> save();
@@ -116,6 +125,17 @@ public function actualizarMantenimiento(Request $request){
 
   
 }
+
+public function guardarFacturaMantenimiento(Request $request){
+  $actu = Mantenimiento::find($request->data['id_m']);
+  $actu->numero_factura = $request->data['numero_facturaM'];
+  $actu->fecha_facturacion = $request->data['fecha_facturacionM'];
+  $actu->precio = $request->data['precio_mantenimientoM'];
+  $actu->descripcion = $request->data['descripcion_mantenimiento'];
+  $agregar = $actu -> save();
+  return response()->json('actualizado con exito'); 
+}
+
 
 /* Funcion para ver los detalles del mantenimiento*/
 public function detallemantenimento($id){
