93b0c105a3931968eb8fbfb9e972bfccdb92204a
Sucelinda <109007419+Sucelinda@users.noreply.github.com>
2023-01-19 15:27:12 -0600

    Update RegistroMantenimiento.blade.php

--- resources/views/Servicios/RegistroMantenimiento.blade.php
+++ resources/views/Servicios/RegistroMantenimiento.blade.php
@@ -532,19 +532,9 @@ y solo se podra modificar la fecha de entrega en la accion de editar --}}
         document.getElementById('labelswitchestado').innerHTML = "Pendiente";
       }
     });
-  } else { // FECHA
-  window.onload = function(){
-      var fecha = new Date(); //Fecha actual
-      var mes = fecha.getMonth()+1; //obteniendo mes
-      var dia = fecha.getDate(); //obteniendo dia
-      var ano = fecha.getFullYear(); //obteniendo año
-      if(dia<10)
-        dia='0'+dia; //agrega cero si el menor de 10
-      if(mes<10)
-        mes='0'+mes //agrega cero si el menor de 10
-      document.getElementById('fecha_ingreso').value= ano+"-"+mes+"-"+dia;
-
-    }
+  } else{
+    var fecha = new Date();
+    document.getElementById("fecha_ingreso").value = fecha.toJSON().slice(0,10);  
   }
 
 
@@ -834,7 +824,10 @@ y solo se podra modificar la fecha de entrega en la accion de editar --}}
     myModalbuscarCliente.hide(); 
   }
 
-  
+
+
+var fecha1 = new Date();
+document.getElementById("fecha_facturacion").value = fecha1.toJSON().slice(0,10);
  
 </script>
 
