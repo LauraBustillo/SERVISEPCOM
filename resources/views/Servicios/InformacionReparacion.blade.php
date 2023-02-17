c333c0034ec40d0c1747253c8e369826eae3c65a
LauraBustillo <93621801+LauraBustillo@users.noreply.github.com>
2023-02-16 21:25:05 -0600

    Update InformacionReparacion.blade.php

--- resources/views/Servicios/InformacionReparacion.blade.php
+++ resources/views/Servicios/InformacionReparacion.blade.php
@@ -211,7 +211,7 @@
 
 
 
- <!-- Modal de dialogo de Buscar cliente --> 
+ <!-- Modal de fotos--> 
  <div class="modal fade"  id="modalverfotosreparacion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog  modal-xl" >
     <div class="modal-content">
