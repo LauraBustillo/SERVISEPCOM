b509101031471be6a7f9b65f708ccc2c6a593dfb
Sucelinda <109007419+Sucelinda@users.noreply.github.com>
2023-02-16 21:25:59 -0600

    Update lista.blade.php
    Actualización Listado

--- resources/views/rangofactura/lista.blade.php
+++ resources/views/rangofactura/lista.blade.php
@@ -88,6 +88,13 @@
 
 </style>
 
+@if (session('mensaje'))
+  <script>
+    mensaje = {!! json_encode(session('mensaje'), JSON_HEX_TAG) !!};
+    alertify.success(mensaje);
+  </script> 
+@endif 
+
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
 
