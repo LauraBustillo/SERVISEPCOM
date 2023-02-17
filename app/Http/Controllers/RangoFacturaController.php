43e9892f3a0fd5807099d269ecfa2e2305c26a39
Sucelinda <109007419+Sucelinda@users.noreply.github.com>
2023-02-16 20:26:54 -0600

    Rango_Factura

---
+++ app/Http/Controllers/RangoFacturaController.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RangoFactura;
+use App\Http\Requests\StoreRangoFacturaRequest;
+use App\Http\Requests\UpdateRangoFacturaRequest;
+use Illuminate\Http\Request;
+
+class RangoFacturaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $rangos = RangoFactura::all()->sortByDesc('id');
+        $rangoActual = RangoFactura::where('estado', '=', 1)->get();
+        return view('rangofactura.lista')->with('rangos', $rangos)
+            ->with('rangoActual', $rangoActual);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('rangofactura.registrorangofactura');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreRangoFacturaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    //Funcion para guadar
+    public function store(Request $request)
+    {
+
+
+        $rules = ([
+
+            'caiRango' => 'required|unique:rango_facturas|regex:^\b([0-9A-Fa-f]{6}-){5}[0-9A-Fa-f]{2}\b^|max:37',
+            'fechaInicio' => 'required',
+            'fechaVencimiento' => 'required',
+            'facturaInicial' => 'required|regex:^[0-9]-\d{3}-\d{2}-\d{8}$^|max:19',
+            'facturaFinal' => 'required|regex:^[0-9]-\d{3}-\d{2}-\d{8}$^|max:19',
+
+        ]);
+        $mesaje = ([
+
+            'caiRango.required' => 'El CAI es obligatorio',
+            'caiRango.regex' => 'El numero CAI solo adminte valores hexadecimales (0-F)',
+            'caiRango.unique' => 'El numero CAI ya ha sido usado',
+
+
+            'fechaInicio.required' => 'La fecha de inicio es obligatoria',
+
+            'fechaVencimiento.required' => 'La fecha final es obligatoria',
+
+            'facturaInicial.required' => 'La factura inicial es obligatoria',
+
+            'facturaFinal.required' => 'La factura final es obligatoria',
+
+
+            
+
+
+
+        ]);
+        $this->validate($request, $rules, $mesaje);
+
+        $actual = RangoFactura::where('estado', 1)->get();
+        if(count($actual)>0){
+            $actual[0]->estado = 0;
+            $actual[0]->save();
+        }
+
+
+
+        $agregar = new RangoFactura();
+        $agregar->fechaInicio = $request->input('fechaInicio');
+        $agregar->fechaVencimiento = $request->input('fechaVencimiento');
+        $agregar->facturaInicial = substr($request->input('facturaInicial'), -8);
+        $agregar->facturaFinal =  substr($request->input('facturaFinal'), -8);
+        $agregar->caiRango =  $request->input('caiRango');
+        $agregar->facturaActual =  substr($request->input('facturaInicial'), -8);
+        $agregar->ocupadas =  0;
+        $agregar->facturaDisponibles = intval(substr($request->input('facturaFinal'), -8)) - intval(substr($request->input('facturaInicial'), -8));
+        $agregar->estado =  1;
+        $agregar->save();
+
+        return redirect()->route('RangoFactura.index')->with('mensaje', 'Se guardó  con  éxito');
+    }
+
+    public function storeVentas(Request $request)
+    {
+
+        $rules = ([
+            'fecha_desde' => 'required',
+            'fecha_hasta' => 'required',
+            'rango_hasta' => 'required|regex:^[0-9]-\d{3}-\d{2}-\d{8}$^|max:19',
+            'rango_desde' => 'required|regex:^[0-9]-\d{3}-\d{2}-\d{8}$^|max:19',
+            'numero_cai' => 'required|regex:^\b([0-9A-Fa-f]{6}-){5}[0-9A-Fa-f]{2}\b^|max:37',
+        ]);
+        $mesaje = ([
+            'fecha_desde.required' => 'La fecha de inicio es requerida',
+            'fecha_hasta.required' => 'La fecha final es requerida',
+            'rango_desde.required' => 'La factura iniciales requerida',
+            'rango_hasta.required' => 'La factura final es requerida',
+            'numero_cai.required' => 'El CAI es requerida',
+            'numero_cai.regex' => 'El numero CAI solo adminte valores hexadecimales (0-F)',
+        ]);
+
+        $this->validate($request, $rules, $mesaje);
+
+
+        $actual = RangoFactura::where('estado', 1)->first();
+        if($actual){
+            $actual->estado = 0;
+            $actual->save();
+        }
+
+        $agregar = new RangoFactura();
+        $agregar->fechaInicio = $request->input('fecha_desde');
+        $agregar->fechaVencimiento = $request->input('fecha_hasta');
+        $agregar->facturaInicial = substr($request->input('rango_desde'), -8);
+        $agregar->facturaFinal =  substr($request->input('rango_hasta'), -8);
+        $agregar->caiRango =  $request->input('numero_cai');
+        $agregar->facturaActual =  substr($request->input('rango_desde'), -8);
+        $agregar->ocupadas = 0;
+        $agregar->facturaDisponibles =  intval(substr($request->input('rango_hasta'), -8)) - intval(substr($request->input('rango_desde'), -8));
+        $agregar->estado = 1;
+        $agregar->save();
+
+        return redirect()->route('show.registroventa')->with('mensaje', 'Se guardó  con  éxito');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RangoFactura  $rangoFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RangoFactura $rangoFactura)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RangoFactura  $rangoFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RangoFactura $rangoFactura)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateRangoFacturaRequest  $request
+     * @param  \App\Models\RangoFactura  $rangoFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRangoFacturaRequest $request, RangoFactura $rangoFactura)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RangoFactura  $rangoFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RangoFactura $rangoFactura)
+    {
+        //
+    }
+}
---
+++ resources/views/rangofactura/registrorangofactura.blade.php
@@ -0,0 +1,188 @@
+@extends('main')
+@section('extra-content')
+
+
+<style>
+    /*Cajas de texto*/
+    .form-control {
+        background-color: transparent;
+        border: 1.3px solid #000000;
+    }
+
+    /*Las label*/
+    .input-group-text {
+        background-color: #000000;
+        border: 1.3px solid #000000;
+        font-family: 'Open Sans';
+        color: #FFFFFF;
+
+    }
+
+    /*Los titulos */
+    .titulo {
+        font: italic normal bold normal 3em/1 Helvetica, Arial, sans-serif;
+        color: black;
+        font-family: 'Open Sans';
+        font-size: 20xp;
+    }
+
+    /*Los botones*/
+    .btn-outline-dark {
+        background-color: transparent;
+        border: 1.8px solid #000000;
+    }
+
+    a {
+        color: aliceblue;
+        outline: none;
+        text-decoration: none;
+        color: #000000;
+    }
+
+    .a:hover {
+        color: white;
+    }
+
+</style>
+
+{{--Mostrar funcion--}}
+<script>
+    var errores = []
+    errores = {!! json_encode($errors->all(), JSON_HEX_TAG) !!};
+    if(errores.length > 0){
+      errores.forEach(element => {
+        alertify.error(element)
+      });
+    }
+
+  </script>
+
+<br>
+<br>
+<form class="form-control" method="POST" action="{{ route('store.rangofactura') }}">
+    @csrf
+    <br>
+    <br>
+
+    {{-- Título --}}
+    <H1 class="titulo" style="text-align: center;">Registrar rango factura</H1>
+    <br>
+    <br>
+
+
+    <div class="input-group input-group-sm flex">
+        {{-- Fecha Inicio --}}
+        <div class="col">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Número CAI</span>
+            <input  type="text" name="caiRango" id="caiRango" class="form-control" value="{{old('caiRango')}}" maxlength="37">
+        </div>
+    </div>
+    <br>
+    <div class="input-group input-group-sm flex">
+        {{-- Fecha Inicio --}}
+        <div class="col">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Fecha Inicio</span>
+            <input type="date" name="fechaInicio" id="fechaInicio" class="form-control" value="{{old('fechaInicio')?old('fechaInicio'):Carbon\Carbon::now()->timezone('America/Tegucigalpa')->format('Y-m-d')}}">
+        </div>
+        &nbsp;&nbsp;&nbsp;
+        {{-- Fecha Vencimiento --}}
+        <div class="col">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Fecha Vencimiento</span>
+            <input type="date" name="fechaVencimiento" id="fechaVencimiento" class="form-control" value="{{old('fechaVencimiento')}}">
+        </div>
+    </div>
+
+    <br>
+    <div class="input-group input-group-sm flex">
+        {{-- Factura Inicial --}}
+        <div class="col">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Factura Inicial</span>
+            <input type="text" name="facturaInicial" id="facturaInicial" class="form-control" value="{{ old('facturaInicial') }}" placeholder="Ej.000-000-00-00000000" maxlength="19">
+        </div>
+        &nbsp;&nbsp; &nbsp;
+        {{-- Factura Final --}}
+        <div class="col">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Factura Final</span>
+            <input type="text" name="facturaFinal" id="facturaFinal" class="form-control" value="{{old('facturaFinal')}}" placeholder="Ej.000-000-00-00000000" maxlength="19">
+        </div>
+    </div>
+    <br><br>
+
+    <script>
+        var d = new Date();
+        var offset = -6; // offset para la hora de Honduras en GMT
+        d.setHours(d.getHours() + offset);
+        document.getElementById("fechaInicio").min = d.toISOString().split("T")[0];
+        d.setMonth(d.getMonth() + 1);
+        document.getElementById("fechaVencimiento").min = d.toISOString().split("T")[0];
+
+        function validar(event) {
+            // se obtiene el valor actual del carácter presionado en el teclado y se almacena en la variable value
+            var value = event.key;
+            //se obtiene el valor actual del elemento facturaFinal y se almacena en la variable texto
+            var input = this;
+            var texto = input.value;
+            /*
+                La función isNaN se utiliza para determinar si un valor es un
+                número o no. parseInt se utiliza para convertir una cadena en
+                un número entero. Si el valor actual no es un número,
+                se cancela el evento predeterminado
+                (utilizando event.preventDefault()) y se detiene la ejecución
+                del controlador de eventos
+            */
+            if (isNaN(parseInt(event.key))) {
+                event.preventDefault();
+                return;
+            }
+            /*
+                se comprueba si la longitud del texto actual es 3, 7 o 10 caracteres y,
+                si es así, se agrega un guion al final del texto
+                para simular el formato especificado por la SAR (XXX-XXX-XX-XXXXXXXX)
+            */
+            if (texto.length == 3 || texto.length == 7 || texto.length == 10) {
+                texto += '-';
+                /*
+                    valor resultante con los guiones se asigna
+                    de nuevo al elemento facturaFinal y se mostrará
+                    en el campo de texto HTML.
+                */
+                input.value = texto;
+            }
+        }
+        //se agrega un controlador de eventos para el evento keypress en el elemento con id facturaFinal
+        document.getElementById("facturaFinal").addEventListener("keypress", validar);
+        //se agrega un controlador de eventos para el evento keypress en el elemento con id facturaInicial
+        document.getElementById("facturaInicial").addEventListener("keypress", validar);
+
+
+        function validarCai(event) {
+            var value = event.key;
+            var input = this;
+            var texto = input.value;
+            if (isNaN(parseInt(event.key,16))) {
+                event.preventDefault();
+                return;
+            }
+            if (texto.length == 6 || texto.length == 13 || texto.length == 20 || texto.length == 27 || texto.length == 34) {
+                texto += '-';
+                input.value = texto;
+            }
+        }
+
+        document.getElementById("caiRango").addEventListener("keypress", validarCai);
+
+
+    </script>
+
+    {{--Botones --}}
+    <center>
+        <button class="btn btn-outline-dark" type="submit"> <i class="bi bi-folder-fill"> Guardar</i></button>
+        <button type="reset" class="btn btn-outline-dark"> <i class="bi bi-eraser-fill"> Limpiar</i></button>
+        <button type="button" class="btn btn-outline-dark"><a class="a" href="{{route('RangoFactura.index')}}"><i class="bi bi-x-circle-fill"> Cerrar</i> </a></button>
+    </center>
+</form>
+
+
+@endsection
+@include('common')
+
