f2215e42c1f70f3953742c9ecce4da66983a1491
Sucelinda <109007419+Sucelinda@users.noreply.github.com>
2023-02-19 23:26:06 -0600

    validaciones de fechas para rango

---
+++ app/Rules/FacturaFinalMayorQueInicialVentas.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class FacturaFinalMayorQueInicialVentas implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $facturaFinal = substr($value, -8);
+        $facturaInicial = substr(request()->input('facturaInicial'), -8);
+
+        return intval($facturaFinal) > intval($facturaInicial);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'El número de factura final debe ser mayor que el número de factura inicial.';
+    }
+}
