406005ecfac23f2632fb0e33a87e4cd061ee038f
MarioOrrego <mario.orrego@unah.hn>
2023-03-16 21:36:13 -0600

    .

---
+++ database/migrations/2023_03_14_004631_create_gastos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGastosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gastos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_gasto');
+            $table->string('tipo_gasto');
+            $table->string('descripcion_gasto');
+            $table->string('total_gasto');
+            $table->string('fecha_gasto');
+            $table->string('responsable_gasto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gastos');
+    }
+}
