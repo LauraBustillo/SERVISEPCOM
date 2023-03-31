2fdcea50ea0861da72c2361be61a467682fbc9ee
EvaHerrera1 <93700725+EvaHerrera1@users.noreply.github.com>
2023-03-30 18:44:22 -0600

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,12 @@
 @extends('main')
 @section('extra-content')
 
+@if (session('denegar'))
+  <script>
+    mensaje = {!! json_encode(session('denegar'), JSON_HEX_TAG) !!};
+    alertify.success(mensaje);
+  </script>
+@endif
+
 @endsection
 @include('common')
